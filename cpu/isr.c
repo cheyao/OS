73d9bce3ae0ce5ed8c5661e5ff313b1cc1b3e2bf
cyao1234 <chengyin.yao@live.com>
2022-07-14 12:41:29 +0200

    n

--- cpu/isr.c
+++ cpu/isr.c
@@ -2,7 +2,7 @@
 #include "../kernel/functions.h"
 
 void int_handler(u8int error, u8int interupt) {
-    //char* str = "0";    // Turns one digit ints to strings, enough for now
-    //*str += exception;
-    //kprint(str);// Prints the int
+    char* str = "0";    // Turns one digit ints to strings, enough for now
+    *str += interupt;
+    kprint(str);// Prints the int
 }
